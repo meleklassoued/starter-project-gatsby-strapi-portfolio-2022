e1fbda7954a09b58b784770eb2e9d324bcc66d96
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-17 21:07:02 +0100

    fix: :white_check_mark: fix plugin config syntax

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,7 @@ module.exports = {
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
-        collectionTypes: [`jobs`],
-        singleTypes: [],
+        collectionTypes: [`/jobs`],
       },
     },
     // {
