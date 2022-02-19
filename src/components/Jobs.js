b07939c352d46e7e8fdc56f690bb9c7f093d7a59
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-19 20:33:50 +0100

    feat: :white_check_mark: test the functionnality is it done with success

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -21,7 +21,35 @@ const query = graphql`
   }
 `
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const Data = useStaticQuery(query)
+  const {
+    allStrapiApiJobs: { nodes: data },
+  } = Data
+  const [value, setValue] = React.useState(0)
+
+  console.log(...data)
+  console.log(data[0].data[value].attributes)
+  const { company, date, description, position } = data[0].data[
+    value
+  ].attributes
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="job-center">
+        <div className="btn-container">
+          {data[0].data.map((item, index) => {
+            const { company, date, description, position } = item.attributes
+            return (
+              <div key={index}>
+                <h1>{company}</h1>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Jobs
