cc268ae9bc82b38cff779221d88b0040e708c1c0
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-21 12:19:42 +0100

    feat: :construction: fetch Projects data
    query dataProjects and pass it to the projects props

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,54 @@ import Services from "../components/Services"
 import Jobs from "../components/Jobs"
 import Projects from "../components/Projects"
 import Blogs from "../components/Blogs"
-export default () => {
+export default ({ data }) => {
+  console.log(data)
+  const {
+    allStrapiApiProjectsPopulateStacksImages: {
+      nodes: [Array],
+    },
+  } = data
+  const dataStrapi = Array.data[0].attributes
+  console.log(dataStrapi)
   return (
     <>
       <Layout>
         <Hero />
         <Services />
         <Jobs />
+        <Projects projects={dataStrapi} title="featured projects" showLink />
       </Layout>
     </>
   )
 }
+export const query = graphql`
+  {
+    allStrapiApiProjectsPopulateStacksImages(
+      filter: {
+        data: { elemMatch: { attributes: { featured: { eq: true } } } }
+      }
+    ) {
+      nodes {
+        data {
+          attributes {
+            github
+            description
+            title
+            images {
+              data {
+                attributes {
+                  url
+                }
+              }
+            }
+            stacks {
+              stack_name
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`
 // ...GatsbyImageSharpFluid
