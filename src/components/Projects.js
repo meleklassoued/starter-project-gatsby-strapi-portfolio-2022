9149c1102f28d3aacc9dedcba70e03063aa33b2b
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-21 13:30:29 +0100

    feat: :construction: setup projects component
    maping the data from the query into project component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,8 +2,23 @@ import React from "react"
 import Title from "./Title"
 import Project from "./Project"
 import { Link } from "gatsby"
-const Projects = () => {
-  return <h2>projects list</h2>
+const Projects = ({ projects, title, showLink }) => {
+  return (
+    <section className="section projects ">
+      <Title title={title} />
+      <div className="section-center projects-center">
+        {projects.map((project, index) => {
+          console.log()
+          return <Project key={index} index={index} projectInfo={project} />
+        })}
+      </div>
+      {showLink && (
+        <Link to="/projects" className="btn center-btn">
+          show more Projects
+        </Link>
+      )}
+    </section>
+  )
 }
 
 export default Projects
