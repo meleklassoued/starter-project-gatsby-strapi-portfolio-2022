aac5be6d8708cea42a10d71cfc07bae60051d804
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2022-02-21 13:33:23 +0100

    feat: :construction: spread out props and begin the construction of the component

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,8 +2,13 @@ import React from "react"
 import PropTypes from "prop-types"
 import Image from "gatsby-image"
 import { FaGithubSquare, FaShareSquare } from "react-icons/fa"
-const Project = () => {
-  return <h2>project copmonent</h2>
+const Project = ({ projectInfo, index }) => {
+  const { description, github, images, stacks, title } = projectInfo.attributes
+  return (
+    <article className="projects">
+      <img src={images.data.attributes.url} className="project-img" />
+    </article>
+  )
 }
 
 Project.propTypes = {}
